1b40175ca2481a53e0191eb456e8f6ce0462bd55
Dylan Pierce <dylanpierce914@gmail.com>
2012-10-31 21:51:43 -0500

    Version 0.2
    Changes:
    * Modulated part of grab_music into check_for_songs to break the
    iteration of wwWednesday and ssSunday in case of an error
    * added Generic_Blog module for blogs without lists
    
    Needed Changes:
    * a list of pre-downloaded URLs before grab_music is activiated to
    check for duplicates and break if there is one

--- musiccrawler.py
+++ musiccrawler.py
@@ -1,8 +1,29 @@
 import urllib, os, re, csv, random, urllib2, eyed3
-os.chdir('/Users/Pierce/Documents/Programming')
+
+####### Variables ######
+Mp3Crawl_dir = '/Users/Pierce/Mp3Crawl'
+Music_dir = '/Users/Pierce/Music/Captured'
+
+
+os.chdir(Mp3Crawl_dir)
 print 'Musiccrawler.py loaded'
+
+
+def check_for_songs(url):
+    page = urllib.urlopen(url).read()
+    print "Page retrieved succesfully"
+    songs = re.findall(r'href=[\'"]?([^\'" >]+.mp3)', page)
+    print str(len(songs)) + " songs found"
+    if len(songs) == 0:
+        return False
+    else:
+        return True
+    
+
+
+
 def changefilename(song): #module to change the filename of the mp3 when it's finished downloding. Earmilk uses a strange filenaming convention
-    os.chdir('/Users/Pierce/Documents/Programming')
+    os.chdir(Music_dir)
     audiofile = eyed3.load(song)
     print audiofile.tag.artist
     print audiofile.tag.title
@@ -11,38 +32,41 @@ def changefilename(song): #module to change the filename of the mp3 when it's fi
 
 
 def grab_music(url):
-    #url = 'http://www.earmilk.com/2012/09/26/wobble-wednesday-73/'  #change this to a variable once this whole script is ready to be used as a module
-    page = urllib.urlopen(url).read()
-    print "Page retrieved succesfully"
-    songs = re.findall(r'href=[\'"]?([^\'" >]+.mp3)', page)
-    print str(len(songs)) + " songs found"
-    title = 21   #iteration used to name the files when they are downloading, once they are finished, then changefilename() will be used to change to the actual name
-    for song in songs:
-     #experiment with filename
-        file_name = str(title) + ".mp3"  #song.split('/')[-1]
-        u = urllib2.urlopen(song)
-        f = open(file_name, 'wb')
-    #script to display download progress#
-        meta = u.info()
-        file_size = int(meta.getheaders("Content-Length")[0])
-        print "Downloading: %s Bytes: %s" % (file_name, file_size)
-        file_size_dl = 0
-        block_sz = 8192
-        while True:
-            buffer = u.read(block_sz)
-            if not buffer:
-                break
-            file_size_dl += len(buffer)
-            f.write(buffer)
-            status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-            status = status + chr(8)*(len(status)+1)
-            print status,
+    if check_for_songs(url) is False:
+        return False
+    else:
+        os.chdir(Music_dir)
+        title = 21   #iteration used to name the files when they are downloading, once they are finished, then changefilename() will be used to change to the actual name
+        page = urllib.urlopen(url).read()
+        print "Page retrieved succesfully"
+        songs = re.findall(r'href=[\'"]?([^\'" >]+.mp3)', page)
 
-        f.close()
-        #title iteration
-        title = title + 1
-        #call to change the file name from arbitiary number to actual song name
-        changefilename(file_name)
+        for song in songs:
+         #experiment with filename
+            file_name = str(title) + ".mp3"  #song.split('/')[-1]
+            u = urllib2.urlopen(song)
+            f = open(file_name, 'wb')
+        #script to display download progress#
+            meta = u.info()
+            file_size = int(meta.getheaders("Content-Length")[0])
+            print "Downloading: %s Bytes: %s" % (file_name, file_size)
+            file_size_dl = 0
+            block_sz = 8192
+            while True:
+                buffer = u.read(block_sz)
+                if not buffer:
+                    break
+                file_size_dl += len(buffer)
+                f.write(buffer)
+                status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+                status = status + chr(8)*(len(status)+1)
+                print status,
+
+            f.close()
+            #title iteration
+            title = title + 1
+            #call to change the file name from arbitiary number to actual song name
+            changefilename(file_name)
 
 
 
--- urlgenerator.py
+++ urlgenerator.py
@@ -3,31 +3,42 @@ import urllib, datetime, os, musiccrawler, re, csv, random, urllib2, eyed3
 today = datetime.datetime.today()       #set global time
 print 'url generator'
 weekday = today.isoweekday()  #returns an integer corresponding with the weekday, 1 == Monday etc. etc.
+####### Variables ######
+Mp3Crawl_dir = '/Users/Pierce/Mp3Crawl'
+Music_dir = '/Users/Pierce/Music/Captured'
 
 
-#Thissongissick
+#### ChubbyBeavers, other sites without schedules #####
+def GenericSites():
+    #no good list
+    #Beathau5.com
+    urls = ['http://www.chubbybeavers.com/','http://bacauhousemafia.ro/']
+    for url in urls:
+        musiccrawler.grab_music(url)
+
+##### Thissongissick #####
         
 
-#Earmilk
+##### Earmilk #####
 
 def wwWednesday():
     'Today is Wednesday!'
-    wwcount = open("//Users/Pierce/Documents/Programming/wwCount.txt", "r")
+    wwcount = open(str(Mp3Crawl_dir)+'/wwCount.txt', "r")
     #ww.write(wwcount+1)
     wwnumber = wwcount.readline()
     wwcount.close()
-    url = 'http://www.earmilk.com/' + str(today.year) + '/' + str(today.month) + '/' + str(today.day) + '/wobble-wednesday/' +  str(wwnumber) + '/'
+    url = 'http://www.earmilk.com/' + str(today.year) + '/' + str(today.month) + '/' + str(today.day) + '/wobble-wednesday-' +  str(wwnumber) + '/'
     print url
     
     #actually start downloading music
-    #if grab_music() doesn't finds 0 songs it will return a 0
-    if int(musiccrawler.grab_music(url)) == 0:
-        return   #return out of the function without changing the contents of wwCount.xt
-    
+    #if there are 0 songs on page, a helper module (check_for_songs) will catch and break the program)
+
+    if musiccrawler.grab_music(url) is False:
+        return
     else:
         musiccrawler.grab_music(url)
-
-    wwcount = open("/Users/Pierce/Documents/Programming/wwCount.txt", "w+")
+        
+    wwcount = open(str(Mp3Crawl_dir) +'/wwCount.txt', "w+")
     wwnumber = int(wwnumber) + 1
     print 'currently on WobbleWednesday #' + str(wwnumber)
     print 'saving number to wwCount.txt...'
@@ -37,7 +48,7 @@ def wwWednesday():
 def ssSunday():
     'Today is Sunday!'
     #open text file that contains the count of the current Suicide Sunday (>109)
-    sscount = open("//Users/Pierce/Documents/Programming/ssCount.txt", "r")
+    sscount = open(str(Mp3Crawl_dir) + "/ssCount.txt", "r")
     #ww.write(wwcount+1)
     ssnumber = sscount.readline()
     sscount.close()
@@ -46,13 +57,10 @@ def ssSunday():
     
     #actually start downloading music
     #if grab_music() doesn't finds 0 songs it will return a 0
-    if int(musiccrawler.grab_music(url)) == 0:
-        return   #return out of the function without changing the contents of wwCount.xt
-    
-    else:
-        musiccrawler.grab_music(url)
 
-    sscount = open("/Users/Pierce/Documents/Programming/ssCount.txt", "w+")
+    musiccrawler.grab_music(url)
+
+    sscount = open(str(Mp3Crawl_dir) + "/ssCount.txt", "w+")
     ssnumber = int(ssnumber) + 1
     print 'currently on WobbleWednesday #' + str(ssnumber)
     print 'saving number to ssCount.txt...'
