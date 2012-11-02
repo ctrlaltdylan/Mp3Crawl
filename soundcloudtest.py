5b50abf830f57abcac5de9105de45b06fbe0f92f
Dylan Pierce <dylanpierce914@gmail.com>
2012-11-02 17:49:39 -0500

    Version 0.3.1
    Changes:
    *Soundcloud.py's find_music function works. It searches for '*.mp3'
    URL's as well as '/download' URL's (the Soundcloud standard) and
    appends them to a list
    
    What's Next:
    * Trying to replace the regular method of downloading with the
    soundcloud which is much much better

--- soundcloudtest.py
+++ soundcloudtest.py
@@ -1,10 +1,21 @@
-import urllib, os
+import urllib, os, re
 
-os.chdir('/Users/Pierce/Music')
-url = 'http://soundcloud.com/nickraymondg/bro-safari-feat-dj-craze/download'
-resp = urllib.urlopen(url)
-image_data = resp.read()
-# Open output file in binary mode, write, and close.
-f = open('test.mp3','wb')
-f.write(image_data)
-f.close()
+
+def find_songs(url):
+    page = urllib.urlopen(url).read()
+    print page
+    songs = re.findall(r'href=[\'"]?([^\'" >]+/download)', page)
+    songs.append(re.findall(r'href=[\'"]?([^\'" >]+.mp3)', page))
+    print songs
+    #songs.append(re.findall('rhref=[\'"]?([^\'" >]+.mp3)'
+
+def download_songs(url):
+    os.chdir('/Users/Pierce/Music')
+    resp = urllib.urlopen(url)
+    image_data = resp.read()
+    # Open output file in binary mode, write, and close.
+    f = open('test.mp3','wb')
+    f.write(image_data)
+    f.close()
+
+
